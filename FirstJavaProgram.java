871b3624266048df06447cf0515b45a0f3ea5b58
Taylor Guske <taylorguske6191@outlook.com>
2020-09-03 14:58:49 -0700

    FirstJavaProgram.java/FirstJavaProgram.class

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String myName = "Taylor Guske";
+        int myAge = 29;
+        String myTown = "Mill Creek, Wa";
+        System.out.println("My name is " + myName);
+        System.out.println("I am " + myAge + " years old");
+        System.out.println("My hometown is " + myTown);
+    }
+}
