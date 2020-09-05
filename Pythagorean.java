8c91bc0899d45642a8808e19954c65b8320c4774
Taylor Guske <69264858+TGuske6191@users.noreply.github.com>
2020-09-04 18:14:05 -0700

    Delete Pythagorean.java

--- Pythagorean.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Math;
-public class Pythagorean {
-    public static void main (String[] args) {
-        System.out.println (calculateHypotenuse(6, 8));
-    }
-    static double calculateHypotenuse (double legA, double legB) {
-        return Math.sqrt (Math.pow(legA, 2) + Math.pow(legB, 2));
-    }
-}
